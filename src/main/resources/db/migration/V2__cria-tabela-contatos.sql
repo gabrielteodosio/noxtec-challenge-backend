ccb6fcfa35751e54fba46cfc212aa0d2f4a01023
Gabriel T. Marques <gabrieltmm@gmail.com>
2025-02-21 05:53:00 -0300

    fix: migration minor adjustments to make usuario_id not null and accept contato_celular empty

--- src/main/resources/db/migration/V2__cria-tabela-contatos.sql
+++ src/main/resources/db/migration/V2__cria-tabela-contatos.sql
@@ -2,12 +2,12 @@ CREATE TABLE contatos (
     contato_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     contato_nome VARCHAR(100) NOT NULL,
     contato_email VARCHAR(255),
-    contato_celular VARCHAR(11) NOT NULL,
+    contato_celular VARCHAR(11),
     contato_telefone VARCHAR(10),
     contato_sn_favorito CHARACTER(1),
     contato_sn_ativo CHARACTER(1),
     contato_dh_cad TIMESTAMP WITHOUT TIME ZONE,
     contato_dh_ead TIMESTAMP WITHOUT TIME ZONE,
-    usuario_id UUID,
+    usuario_id UUID NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id) ON DELETE CASCADE
 );
