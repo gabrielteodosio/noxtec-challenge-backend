b193d72f4462bd345f9f465c44dee004a59bdbd6
Gabriel T. Marques <gabrieltmm@gmail.com>
2025-02-20 21:47:31 -0300

    added the migrations

---
+++ src/main/resources/db/migration/V1__cria-tabela-usuarios.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuarios (
+    usuario_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    usuario_nome VARCHAR(100) NOT NULL,
+    usuario_email VARCHAR(255) UNIQUE,
+    usuario_senha VARCHAR(255),
+    usuario_dh_cad TIMESTAMP WITHOUT TIME ZONE,
+    usuario_dh_ead TIMESTAMP WITHOUT TIME ZONE
+);
