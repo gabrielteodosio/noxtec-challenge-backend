916f56b86c3619bd2face9fecfafc294a48abaea
Gabriel T. Marques <gabrieltmm@gmail.com>
2025-02-22 05:32:50 -0300

    feat: added CORS and fixed Celular to celular

--- src/main/java/br/com/noxtec/challenge_api/dominio/contato/ContatoResponseDTO.java
+++ src/main/java/br/com/noxtec/challenge_api/dominio/contato/ContatoResponseDTO.java
@@ -3,7 +3,7 @@ package br.com.noxtec.challenge_api.dominio.contato;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record ContatoResponseDTO(UUID id, String nome, String email, String Celular, String telefone,
+public record ContatoResponseDTO(UUID id, String nome, String email, String celular, String telefone,
                                  Character favorito, Character ativo, LocalDateTime dataHoraCriacao,
                                  LocalDateTime dataHoraEdicao) {
 }
